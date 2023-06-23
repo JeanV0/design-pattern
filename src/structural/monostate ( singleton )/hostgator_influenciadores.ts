43974e9984fcb0bab2d0deeada03d184a032f1d6
Jean <jean@DESKTOP-C18SOHC>
2023-06-23 14:15:20 -0400

    feat(monostate): comments and example

--- src/structural/monostate ( singleton )/hostgator_influenciadores.ts
+++ src/structural/monostate ( singleton )/hostgator_influenciadores.ts
@@ -1,17 +1,42 @@
 import { Influenciadores } from "./influenciadores";
 
 export class HostGatorInfluenciadores {
-    private members: Influenciadores[] = [];
+    // Variavel "compartilhada"
+    private static members: Influenciadores[] = [];
 
     constructor() {
 
     }
 
+    // Adicionar membros 
     setMembers(...members: Influenciadores[]) {
-        members.forEach(item => this.members?.push(item))
+        members.forEach(item => HostGatorInfluenciadores.members?.push(item))
     }
 
+    // Pegar membros da variavel estatica
     getMembers(): Influenciadores[] {
-        return this.members;
+        return HostGatorInfluenciadores.members;
     }
 }
+
+const hostGator = new HostGatorInfluenciadores();
+
+const hostGator2 = new HostGatorInfluenciadores();
+
+const member = new Influenciadores('Gato', 'Gato galatico')
+
+hostGator.setMembers(member);
+
+console.log(`Instancia são iguais?\nR: ${hostGator == hostGator2}\n`);
+console.log(`Valores estaticos?\nR:${hostGator.getMembers()[0].name}\nR:${hostGator2.getMembers()[0].name}`);
+
+/**
+ * Saida:
+Instancia são iguais?R: false
+
+Valores estaticos?
+R: Gato
+R:Gato
+ */
+
+// Diferente do singleton que quebra SOLID, monostate pode criar até um segurança a mais para uso da variavel estatica
