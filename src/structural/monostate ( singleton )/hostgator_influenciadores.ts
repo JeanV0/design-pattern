9d41fc3e7be04591e1dfc566f20c5d71fe2e6012
Jean <jean@DESKTOP-C18SOHC>
2023-06-23 14:16:00 -0400

    Revert "feat(monostate): comments and example"
    This reverts commit 43974e9984fcb0bab2d0deeada03d184a032f1d6.

--- src/structural/monostate ( singleton )/hostgator_influenciadores.ts
+++ src/structural/monostate ( singleton )/hostgator_influenciadores.ts
@@ -1,42 +1,17 @@
 import { Influenciadores } from "./influenciadores";
 
 export class HostGatorInfluenciadores {
-    // Variavel "compartilhada"
-    private static members: Influenciadores[] = [];
+    private members: Influenciadores[] = [];
 
     constructor() {
 
     }
 
-    // Adicionar membros 
     setMembers(...members: Influenciadores[]) {
-        members.forEach(item => HostGatorInfluenciadores.members?.push(item))
+        members.forEach(item => this.members?.push(item))
     }
 
-    // Pegar membros da variavel estatica
     getMembers(): Influenciadores[] {
-        return HostGatorInfluenciadores.members;
+        return this.members;
     }
 }
-
-const hostGator = new HostGatorInfluenciadores();
-
-const hostGator2 = new HostGatorInfluenciadores();
-
-const member = new Influenciadores('Gato', 'Gato galatico')
-
-hostGator.setMembers(member);
-
-console.log(`Instancia são iguais?\nR: ${hostGator == hostGator2}\n`);
-console.log(`Valores estaticos?\nR:${hostGator.getMembers()[0].name}\nR:${hostGator2.getMembers()[0].name}`);
-
-/**
- * Saida:
-Instancia são iguais?R: false
-
-Valores estaticos?
-R: Gato
-R:Gato
- */
-
-// Diferente do singleton que quebra SOLID, monostate pode criar até um segurança a mais para uso da variavel estatica
